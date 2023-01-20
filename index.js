96cd3174535c8a392dce61e758d4045ac5f2f91e
JonahPeck <PeckJonahC@gmail.com>
2023-01-20 08:22:25 -0700

    commit tree 5193db397cea45da413ebea508bfbf71a6f1d108

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+employeeInfo: {
+    name: "Jonah",
+    streetAddress: "Grand",
+}
+};
+function updateEmployeeWithKeyAndValue (employee, employeeInfo, name) {
+const newEmployee = {...employee};
+newEmployee[employeeInfo] = name;
+return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, employeeInfo, name ){
+employee[employeeInfo] = name;
+return employee;
+}
+function  deleteFromEmployeeByKey(employee, name){
+    const newEmployee = { ...name};
+    newEmployee[employee] = name;
+    return newEmployee;
+
+}
+function  destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
